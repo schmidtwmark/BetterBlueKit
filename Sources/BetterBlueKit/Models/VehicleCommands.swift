78fea01e89acc25847238d122c67bb47c2c0faf6
Mark Schmidt <mark.schmidt@hey.com>
2025-11-29 22:41:38 -0600

    Cleanup stuff

--- Sources/BetterBlueKit/Models/VehicleCommands.swift
+++ Sources/BetterBlueKit/Models/VehicleCommands.swift
@@ -20,24 +20,18 @@ public enum VehicleCommand: Sendable {
                 body = ["airCtrl": options.climate ? 1 : 0,
                         "airTemp": ["value": String(Int(options.temperature.value)),
                                     "unit": options.temperature.units.integer()],
-                        "defrost": options.defrost, "heating1": options.heating ? 1 : 0]
+                        "defrost": options.defrost, "heating1": options.heatValue]
                 if generation >= 3 {
                     body["igniOnDuration"] = options.duration
-                    body["seatHeaterVentInfo"] = ["drvSeatHeatState": options.frontLeftSeat,
-                                                  "astSeatHeatState": options.frontRightSeat,
-                                                  "rlSeatHeatState": options.rearLeftSeat,
-                                                  "rrSeatHeatState": options.rearRightSeat]
+                    body["seatHeaterVentInfo"] = options.getSeatHeaterVentInfo()
                 }
             } else {
                 body = ["Ims": 0, "airCtrl": options.climate ? 1 : 0,
                         "airTemp": ["unit": options.temperature.units.integer(),
                                     "value": Int(options.temperature.value)],
-                        "defrost": options.defrost, "heating1": options.heating,
+                        "defrost": options.defrost, "heating1": options.heatValue,
                         "igniOnDuration": options.duration,
-                        "seatHeaterVentInfo": ["drvSeatHeatState": options.frontLeftSeat,
-                                               "astSeatHeatState": options.frontRightSeat,
-                                               "rlSeatHeatState": options.rearLeftSeat,
-                                               "rrSeatHeatState": options.rearRightSeat],
+                        "seatHeaterVentInfo": options.getSeatHeaterVentInfo(),
                         "username": username, "vin": vin]
             }
         } else if case .startCharge = self {
@@ -51,7 +45,6 @@ public struct ClimateOptions: Codable, Equatable, Sendable {
     public var climate: Bool = true
     public var temperature: Temperature = .init(units: 1, value: "72")
     public var defrost: Bool = false
-    public var heating: Bool = false
     public var duration: Int = 10
     public var frontLeftSeat: Int = 0, frontRightSeat: Int = 0
     public var rearLeftSeat: Int = 0, rearRightSeat: Int = 0
@@ -62,29 +55,70 @@ public struct ClimateOptions: Codable, Equatable, Sendable {
     public var frontRightVentilation: Bool? = false
     public var rearLeftVentilation: Bool? = false
     public var rearRightVentilation: Bool? = false
-    public var rearDefrost: Bool? = true
+    public var rearDefrost: Bool? = false
 
     public init() {}
 
     // Safe getters and setters with default values
-    public var hasFrontLeftVentilation: Bool {
+    public var frontLeftVentilationEnabled: Bool {
         get { frontLeftVentilation ?? false }
         set { frontLeftVentilation = newValue }
     }
-    public var hasFrontRightVentilation: Bool {
+    public var frontRightVentilationEnabled: Bool {
         get { frontRightVentilation ?? false }
         set { frontRightVentilation = newValue }
     }
-    public var hasRearLeftVentilation: Bool {
+    public var rearLeftVentilationEnabled: Bool {
         get { rearLeftVentilation ?? false }
         set { rearLeftVentilation = newValue }
     }
-    public var hasRearRightVentilation: Bool {
+    public var rearRightVentilationEnabled: Bool {
         get { rearRightVentilation ?? false }
         set { rearRightVentilation = newValue }
     }
-    public var hasRearDefrost: Bool {
-        get { rearDefrost ?? true }
+    public var rearDefrostEnabled: Bool {
+        get { rearDefrost ?? false }
         set { rearDefrost = newValue }
     }
+
+    public var heatValue: Int {
+        if !rearDefrostEnabled && steeringWheel == 0 {
+            return 0
+        } else if rearDefrostEnabled && steeringWheel != 0 {
+            return 4
+        } else if rearDefrostEnabled {
+            return 2
+        } else if steeringWheel != 0 {
+            return 3
+        }
+        return 0
+    }
+
+    public func getSeatHeaterVentInfo() -> [String: Int] {
+        return ["drvSeatHeatState":
+                    convertSeatSetting(self.frontLeftSeat, self.frontLeftVentilationEnabled),
+                  "astSeatHeatState":
+                    convertSeatSetting(self.frontRightSeat, self.frontRightVentilationEnabled),
+                  "rlSeatHeatState":
+                    convertSeatSetting(self.rearLeftSeat, self.rearLeftVentilationEnabled),
+                  "rrSeatHeatState":
+                    convertSeatSetting(self.rearRightSeat, self.rearRightVentilationEnabled)]
+    }
+}
+
+// Taken from the Scriptable project
+// Maps the internal ClimateOptions to the values Hyundai/Kia expect
+// https://github.com/andyfase/egmp-bluelink-scriptable/blob/e86ccf383944c88a0c17228a6ddd75e34e9c4dac/src/config.ts#L29
+func convertSeatSetting(_ value: Int, _ cooling: Bool) -> Int {
+    if value == 0 {
+        return 0
+    }
+    // For cooling, 1 -> 3, 2 -> 4, 3 -> 5
+    // For warm, 1 -> 6, 2 -> 7, 3 -> 8
+    if cooling {
+        return value + 2
+    } else {
+        return value + 5
+    }
+
 }
